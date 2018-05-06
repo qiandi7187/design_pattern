98a0a9b33146539a2e3e6d354401d7aa8a3c5c72
qd <441228449@qq.com>
2018-05-06 15:26:16 +0800

    设计模式初版

---
+++ src/com/dahua/observer/WeatherService.java
@@ -0,0 +1,24 @@
+package com.dahua.observer;
+
+/**
+ * 观察者模式  该例子取自大话设计模式  本人添加多个观察者的实现
+ *
+ * 关系四个 ：observable 主题  和继承类    observer 被观察者 和 实现类
+ *
+ */
+public class WeatherService {
+
+    public static void main(String[] args) {
+        Earth earth = new Earth();
+        Satellite satellite =  new Satellite("卫星1");
+        Satellite satellite2 =  new Satellite("卫星2");
+        earth.addObserver(satellite2);
+        earth.addObserver(satellite);
+        earth.setWeather("晴");
+        earth.setWeather("晴");
+        earth.setWeather("多云");
+        earth.setWeather("雨");
+        earth.setWeather("雨");
+    }
+
+}
