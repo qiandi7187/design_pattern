bd16fbfc93a105a905b781d955f2123d9112bed4
qd <441228449@qq.com>
2018-05-09 13:31:38 +0800

    桥接 注释修改

--- src/com/bridge/javaAndPattern/Client.java
+++ src/com/bridge/javaAndPattern/Client.java
@@ -13,7 +13,7 @@ import com.bridge.javaAndPattern.weapen.Sword;
  * 抽象类 和 其实现
  * 接口和 其实现
  * 抽象类内聚接口 ，抽象与接口有关的方法
- * 这种特点让我想到了SrtingMvc中的service 调用 dao层的结构 只不过从底层实现理解并非完全的桥接模式，更偏向代理模式
+ * 这种特点让我想到了SrtingMvc中的service 调用 dao层的结构 只不过从底层实现理解并非完全的桥接模式，更偏向代理模式  而且service 内聚 dao层是在具体实现中而非抽象类
  * 实际上由于必须实现抽象类 失去一定的灵活性
  */
 public class Client {
